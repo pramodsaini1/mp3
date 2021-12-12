52bdfef95bd0c09a8b61fb9b27510538351f6824
Pramod Kumar Saini <69410719+pramodsaini1@users.noreply.github.com>
2021-12-12 12:23:14 +0530

    Create edit_album.php

---
+++ admin/edit_album.php
@@ -0,0 +1,168 @@
+<?php
+if(!isset($_COOKIE["login"])){
+    header("location:index.php");
+  }
+else{
+	$email= $_COOKIE["login"];
+	$conn=mysqli_connect("localhost","root","","mp3");
+	$code = $_GET["code"] ;
+ 	$rs=mysqli_query($conn,"select * from album where code='$code'");
+	if($r=mysqli_fetch_array($rs)){
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+		  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <title>Edit-album</title>
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+         <link href="css/styles.css" rel="stylesheet" />
+     <script src="https://use.fontawesome.com/09901d9403.js"></script>
+  	 
+	 
+	 
+    
+	</head>
+	<style>
+ 
+a{
+  text-decoration:none;
+}
+.fa {
+  padding: 20px;
+  font-size: 25px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+}
+.fa-instagram {
+  background: #125688;
+  color: white;
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+}
+	</style>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.html">Pramod MP3</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fa fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-user-circle"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <?php
+			   include("sidenav.php");
+			?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+					  <div class="row">
+					                          <h1 class="mt-4">Album</h1>
+					  <div class="col-sm-6">
+                        <img src="../album/<?php echo $r["code"]?>.jpg" class="img-fluid">					  
+					  
+					  </div>
+					  <div class="col-sm-6">
+						 
+						<form method="post"  action="update_album.php?code=<?php echo $code?>">
+						<label>Edit-album:</label>
+ 						<input type="text" name="album_name" class="form-control" value="<?php echo $r["album_name"]?>"><br><br>
+ 						 <textarea class="form-control" name="description"><?php echo $r["description"]?></textarea><br>
+ 						  <input type="submit" value="submit" class="btn btn-warning" >
+                          </form>
+						  <form  enctype="multipart/form-data" action="photo_update.php?code=<?php echo $code?>">
+						  <input type="file" name="photo" class="form-control"><br><br>
+						  	<input type="submit" value="submit" class="btn btn-info" >
+						  </form>
+						  </div>
+						  </div>
+                    </div>
+                </main>
+				<br><br>
+								 
+				<br><br><br>
+ 				 
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div>
+	        <center> <h3 style="color:black">Follow Us :  <a href="https://www.facebook.com/profile.php?id=100060203576938" target="_blank"><i class="fa fa-facebook"></i></a> &nbsp;<a href="https://twitter.com/PramodK82377407" target="_blank"><i class="fa fa-twitter"></i></a> &nbsp; <a href="https://www.linkedin.com/in/pramod-kumar-saini-98813b1b4/" target="_blank"><i class="fa fa-linkedin"></i></a>&nbsp; <a href="https://www.instagram.com/pramodkumarsaini12/" target="_blank"><i class="fa fa-instagram"></i></a></h3></center>
+                            
+                        </div>
+						<div class="row"><div class="col-sm-12" style="color:black;">
+          <center><br><a href="#" target="_blank" style="color:black">Terms And Condition</a> &nbsp;<a href="#" target="_blank" style="color:black">Privacy Policy</a></center>
+         <center><br><h3>MP3 © 2021</h3></center><br></div></div>
+		        <center> <p>Made with ❤ By <a href="https://www.linkedin.com/in/pramod-kumar-saini-98813b1b4/ " target="_blank">Pramod Kumar Saini</a></p></center>
+
+  
+  
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
+	<?php
+}
+	 
+}
+?>
