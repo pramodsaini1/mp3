7484e91b7c6d14517cb6469aa3d604b9befa2c79
Pramod Kumar Saini <69410719+pramodsaini1@users.noreply.github.com>
2021-12-12 12:27:52 +0530

    Create sidenav.php

---
+++ admin/sidenav.php
@@ -0,0 +1,41 @@
+ 
+
+
+
+
+<div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+							<a class="nav-link" href="mp3/index.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-home"></i></div>
+                                Home
+                            </a>
+                            <a class="nav-link" href="dashboard.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-dashboard"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Interface</div>
+                            <a class="nav-link collapsed" href="album.php" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fa fa-book"></i></div>
+                                Album
+                             </a>
+                             
+                            <a class="nav-link collapsed" href="song.php" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fa fa-music"></i></div>
+                                Song
+                             </a> 
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                         pramodsaini@gmail.com
+	
+                    </div>
+                </nav>
+            </div>
+			
+			
+			
+ 
